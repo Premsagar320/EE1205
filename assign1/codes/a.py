f6d8c5c5555cb37a8dee36d7a7c6c071d93349b1
Premsagar320 <155876375+Premsagar320@users.noreply.github.com>
2024-01-14 00:17:21 +0530

    Add files via upload

---
+++ assign1/codes/a.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Function to calculate X(n)
+def calculate_x_n(a, r, n):
+    return a * (r ** n) if n >= 0 else 0
+
+# Values for a and r
+a_value = 2
+r_value = 0.5
+
+# Generate values for n
+n_values = np.arange(0, 10, 1)
+
+# Calculate X(n) for each n
+x_n_values = [calculate_x_n(a_value, r_value, n) for n in n_values]
+
+# Plotting
+plt.stem(n_values, x_n_values, basefmt="b-", linefmt="b-", markerfmt="bo")
+plt.xlabel('n')
+plt.ylabel('X(n)')
+plt.title(' X(n) = a.r^n')
+plt.savefig('geometric_sequence_plot.png')
+plt.show()
