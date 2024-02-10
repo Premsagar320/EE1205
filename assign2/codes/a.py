2becbca42831b2be17f44f33e0f4da2d82235e7d
Premsagar320 <155876375+Premsagar320@users.noreply.github.com>
2024-02-10 16:55:54 +0530

    Add files via upload

--- assign2/codes/a.py
+++ assign2/codes/a.py
@@ -1,24 +1,23 @@
 import matplotlib.pyplot as plt
 
-# Read data from the file
-with open("output_data.txt", "r") as file:
-    lines = file.readlines()
-
-# Extracting n and y(n) values
+# Lists to store data
 n_values = []
 y_values = []
-for line in lines[1:]:  # Skip the header line
-    n, y_n = line.split()
-    n_values.append(int(n))
-    y_values.append(float(y_n))
+
+# Read data from the file
+with open("output_data.txt", "r") as file:
+    next(file)  # skip the header line
+    for line in file:
+        n, y = map(float, line.split())
+        n_values.append(int(n))
+        y_values.append(y)
 
 # Plotting
-plt.figure(figsize=(8, 6))
-plt.stem(n_values, y_values, basefmt='b-')
-plt.title("Stem Plot of y(n) vs. n")
-plt.xlabel("n")
-plt.ylabel("y(n)")
-plt.xticks(range(0,16))
+plt.stem(n_values, y_values, markerfmt='o', linefmt='-',basefmt='-')
+plt.title('y(n) vs n')
+plt.xlabel('n')
+plt.ylabel('y(n)')
+plt.xticks(range(0, 16))  # Set x-axis ticks for values from 0 to 15
 plt.grid(True)
 plt.savefig('figure_plot.png')
 plt.show()
--- assign2/codes/data_gen.c
+++ assign2/codes/data_gen.c
@@ -13,14 +13,12 @@ int main() {
     fprintf(file, "n     y(n)\n");
 
     // Given values
-    double x_0 = 20.0; // Initial value of x
-    double d = -1.0;   // Common difference
     double y_n;        // Value of y(n)
 
     // Generate values for n to cover the range from 0 to 15 (inclusive) with a step of 1
     for (int n = 0; n <= 15; n++) {
         // Calculate y(n)
-        y_n = (n + 1) / 2.0 * (x_0 + n * d);
+        y_n = (n + 1) * (40 - n) / 2.0;
 
         // Write to file
         fprintf(file, "%d %lf\n", n, y_n);
