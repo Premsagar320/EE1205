13115fc4540185fb4f28947a04ebad6a092e4e3d
Premsagar320 <155876375+Premsagar320@users.noreply.github.com>
2024-02-17 12:13:14 +0530

    Add files via upload

--- assign2/codes/a.py
+++ assign2/codes/a.py
@@ -1,23 +1,27 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
-# Lists to store data
-n_values = []
-y_values = []
+# import the data from the text file
+data = np.loadtxt("output_data.txt", skiprows=1)
 
-# Read data from the file
-with open("output_data.txt", "r") as file:
-    next(file)  # skip the header line
-    for line in file:
-        n, y = map(float, line.split())
-        n_values.append(int(n))
-        y_values.append(y)
+# clear all the previous figures
+plt.close("all")
 
-# Plotting
-plt.stem(n_values, y_values, markerfmt='o', linefmt='-',basefmt='-')
-plt.title('y(n) vs n')
+# extract the first 20 terms of the data
+n = data[:16, 0]
+y_n = data[:16, 1]
+
+# plot the graph
+plt.stem(n, y_n, markerfmt='bo', linefmt='b-', basefmt='r-',label=r'Simulation') 
+plt.axhline(y=200, color='green', linestyle='--',label='y=200')
+# Set labels and title
 plt.xlabel('n')
 plt.ylabel('y(n)')
-plt.xticks(range(0, 16))  # Set x-axis ticks for values from 0 to 15
+plt.xticks(n)
+plt.title('Simulation v/s Analysis')
+
+# Add legend
+plt.legend()
 plt.grid(True)
-plt.savefig('figure_plot.png')
+plt.savefig("figure_plot.png")
 plt.show()
